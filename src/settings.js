0e51095a41f56a85b6893743b77312e9b8a6f92e
qianer1 <617017837@qq.com>
2022-10-10 14:37:08 +0800

    第一次储存

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+    // 这个地方是修改网站你名字的
+    title: '人力资源管理平台',
+
+    /**
+     * @type {boolean} true | false
+     * @description Whether fix the header
+     */
+    fixedHeader: false,
+
+    /**
+     * @type {boolean} true | false
+     * @description Whether show the logo in sidebar
+     */
+    sidebarLogo: false
+}
